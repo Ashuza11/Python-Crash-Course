c60c18530d27f72a9a184b2decd14876dc1687bd
M ash <ashuza1411@gmail.com>
2022-08-13 05:39:31 +0200

    Execise 9-9 chapter 9

--- chap_9/admin.py
+++ chap_9/admin.py
@@ -5,6 +5,9 @@
 """
 
 # Question 9-5. Login Attempts
+import email
+
+
 class Users():
     """Simulating a user profile"""
     def __init__(self, first_name, last_name, sex, age):
@@ -12,6 +15,7 @@ class Users():
         self.last_name = last_name
         self.sex = sex
         self.age = age
+        self.email = 'dan@ashec.com'
         self.login_attempts = 0
 
     def describe_user(self):
@@ -21,7 +25,8 @@ class Users():
         sex = self.sex
         age = self.age
         login = self.login_attempts
-        print("User profile Info:"+" " + username.title()+", " + name.title()+", " + sex.title()+", "+ str(age)+ ".")
+        email = self.email
+        print("User profile Info:"+" " + username.title()+", " + name.title()+", " + sex.title()+", "+ str(age) +" "+ email + ".")
         print(f"User login {login} times")
 
     def greet_user(self):
@@ -39,6 +44,18 @@ class Users():
         self.login_attempts = 0
 
 
+# Question 9-8. Privileges: 
+class Privileges():
+    """Represent the admin's privileges"""
+    def __init__(self):
+        self.privileges = ["can add post" , "can delete post" , "can ban user"]
+
+    def show_privileges(self):
+        """Show a list of privileges"""
+        for privilege in self.privileges:
+            print("* " + privilege)
+
+
 # Question 9-7. Admin
 
 class Admin(Users):
@@ -46,16 +63,21 @@ class Admin(Users):
     def __init__(self, first_name, last_name, sex, age):
         """Initialize attributes of parentclass."""
         super().__init__(first_name, last_name, sex, age)
-        self.privilages = ["can add post" , "can delete post" , "can ban user"]
-
-    def show_privileges(self):
-        """Show a list of privileges"""
-        for privilege in self.privilages:
-            print("* " + privilege)
-
+        self.email = "dan@gmail.com"
+        self.privilege = Privileges()
 
 
 admin = Admin("Dan", "Ash", "M", 35)
+admin.describe_user()
 print("This is the admin privileges: ")
-admin.show_privileges()
+admin.privilege.show_privileges()
 
+
+
+
+
+# 9-8. Privileges: Write a separate Privileges class. The class should have one
+# attribute, privileges , that stores a list of strings as described in Exercise 9-7.
+# Move the show_privileges() method to this class. Make a Privileges instance
+# as an attribute in the Admin class. Create a new instance of Admin and use your
+# method to show its privileges.
